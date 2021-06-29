ce8d7d2ed9a9aeff4929141d193955315305a307
emotine69 <80795501+emotine69@users.noreply.github.com>
2021-06-29 15:35:16 +0700

    Chỉnh sửa tittle

--- upload.php
+++ upload.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home page</title>
+    <title>Blog</title>
     <link rel="stylesheet" type="text/css" href="style.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
